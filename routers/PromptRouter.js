4e73315eb42fa7fdfde007244e7c7ec8a3c0536b
Nguyễn Quang Dũng - Intern <dunghlhh@gmail.com>
2023-06-28 15:39:31 +0700

    update

--- routers/PromptRouter.js
+++ routers/PromptRouter.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 //get prompt
 router.get('/prompts', PromptController.getAllPrompt);
+router.get('/base_prompts', PromptController.getBasePrompt);
 
 module.exports = router
